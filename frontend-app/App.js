62070699d296f4d9f2b7200e1001808299bcf074
SANGSU0203 <starcarry0203@naver.com>
2025-07-17 14:15:42 +0900

    파일 위치 변경

--- frontend-app/App.js
+++ frontend-app/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import LoginScreen from './consumer/Screen/LoginScreen';
+import LoginScreen from './Main_Login/LoginScreen';
 
 export default function App() {
   return (
--- frontend-app/Main_Login/LoginScreen.js
+++ frontend-app/Main_Login/LoginScreen.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
-import styles from '../Style/LoginStyle';
+import styles from '../consumer/Style/LoginStyle';
 
 const LoginScreen = ({ navigation }) => {
   return (
