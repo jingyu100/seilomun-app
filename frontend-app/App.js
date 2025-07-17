aa72d0ae700cd2f472d814e08132ca394b3d9955
Parkminki-0110 <yuasli0110@g.yju.ac.kr>
2025-07-17 14:27:17 +0900

    로그인 페이지, 메인 페이지 경로 분리

--- frontend-app/App.js
+++ frontend-app/App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import LoginScreen from "./Main_Login/LoginScreen";
 import MainScreen from "./consumer/Screen/MainScreen";
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <>
-      <LoginScreen />
-      <MainScreen />
-      <StatusBar style="auto" />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Main" component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
