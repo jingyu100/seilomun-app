762183402fffb6062dfeb2a0899633562f08d44b
logic-errors <kab03050317@gmail.com>
2025-07-18 12:48:59 +0900

    Update App.js

--- frontend-app/App.js
+++ frontend-app/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import LoginScreen from './Main_Login/LoginScreen';
+import StoreScreen from './consumer/Screen/Store/StoreScreen';
 
 export default function App() {
   return (
     <>
       <LoginScreen />
       <StatusBar style="auto" />
+      <StoreScreen />
     </>
   );
 }
