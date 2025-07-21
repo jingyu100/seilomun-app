af4388208ccab21a8a3f6ac3075b886c400424fd
Parkminki-0110 <yuasli0110@g.yju.ac.kr>
2025-07-22 02:45:41 +0900

    메인 페이지 임박특가 추천 상품 api 구현

--- frontend-app/App.js
+++ frontend-app/App.js
@@ -9,15 +9,14 @@ import MainScreen from "./consumer/Screen/MainScreen";
 import CustomerLoginScreen from "./consumer/Screen/Login/CustomerLoginScreen";
 import StoreScreen from "./consumer/Screen/Store/StoreScreen";
 
-
 const Stack = createNativeStackNavigator();
 
 // 개발중인 화면 보고싶으면 initialRouteName 값 개발중인 화면 name으로 바꾸면 됨
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Store" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} /> 
+      <Stack.Navigator initialRouteName="Main" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="CustomerLogin" component={CustomerLoginScreen} />
         <Stack.Screen name="Store" component={StoreScreen} />
