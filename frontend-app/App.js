ddb847ec7c1710d2fdc510293c6f10ba440e3a79
Parkminki-0110 <yuasli0110@g.yju.ac.kr>
2025-07-23 14:26:28 +0900

    메인 화면 new 상품 api 구현

--- frontend-app/App.js
+++ frontend-app/App.js
@@ -7,7 +7,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./Main_Login/LoginScreen";
 import MainScreen from "./consumer/Screen/MainScreen";
 import CustomerLoginScreen from "./consumer/Screen/Login/CustomerLoginScreen";
-import SellerLoginScreen from "./Seller/Screen/SellerLoginScreen";
+import SellerLoginScreen from "./seller/Screen/SellerLoginScreen";
 import StoreScreen from "./consumer/Screen/Store/StoreScreen";
 import CustomerRegisterScreen from "./consumer/Screen/Login/CustomerRegisterScreen";
 
@@ -16,13 +16,13 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Main" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="CustomerLogin" component={CustomerLoginScreen} />
         <Stack.Screen name="SellerLogin" component={SellerLoginScreen} />
         <Stack.Screen name="Store" component={StoreScreen} />
-        <Stack.Screen name="CustomerRegister" component={CustomerRegisterScreen} /> 
+        <Stack.Screen name="CustomerRegister" component={CustomerRegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
