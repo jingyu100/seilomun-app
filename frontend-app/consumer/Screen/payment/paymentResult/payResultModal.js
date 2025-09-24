0198da76a2d4ba7c7161dbae2fabe9b495412baa
logic-errors <kab03050317@gmail.com>
2025-09-24 20:53:21 +0900

    2025.09.24. 20:53 결제 배달

---
+++ frontend-app/consumer/Screen/payment/paymentResult/payResultModal.js
@@ -0,0 +1,26 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { useRoute } from "@react-navigation/native";
+import {
+    StyleSheet,
+    View,
+    Text,
+    Animated,
+    Image,
+    ScrollView,
+    SafeAreaView,
+    TouchableOpacity,
+    Dimensions,
+    Alert,
+} from 'react-native';
+import api, { S3_BASE_URL } from '../../../../api/api.js';
+
+export default function PayResultModal() {
+
+    
+
+    return(
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
